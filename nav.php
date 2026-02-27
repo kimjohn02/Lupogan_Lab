5eddeb3c9ec11dab7c9053aa386c9e631054ec65
Nieressa <Nieressa@KIMMACLUP>
2026-02-27 20:48:08 +0800

    new folder of my act

---
+++ nav.php
@@ -0,0 +1,10 @@
+<?php // nav.php ?>
+<div style="display:flex; gap:10px; flex-wrap:wrap; margin-bottom:16px;">
+  <a href="/assessment_beginner/index.php">Dashboard</a>
+  <a href="clients_list.php">Clients</a>
+  <a href="services_list.php">Services</a>
+  <a href="bookings_list.php">Bookings</a>
+  <a href="tools_list_assign.php">Tools</a>
+  <a href="payments_list.php">Payments</a>
+</div>
+<hr>
